1e00df868652cd9583c5044cd8fd893c38fc989f
MinhNQ <minhnguyenquang2004vn@gmail.com>
2025-01-02 02:53:48 +0700

    [test] some more test cases

--- TP1.py
+++ TP1.py
@@ -3,9 +3,9 @@ from graphs import *
 if __name__ == "__main__":
     vertex_names = "123456789"
 
-    graph = Graph()
+    g = DirectedGraph()
 
-    graph.add_vertices_by_string(vertex_names)
+    g.add_vertices_by_string(vertex_names)
 
     edge_names = [
         (1, 2),
@@ -25,29 +25,36 @@ if __name__ == "__main__":
         (8, 9),
     ]
 
-    graph.add_edges(edge_names)
+    g.add_edges(edge_names)
 
-    print("Vertices: ", graph.get_vertices())
-    print("Edges: ", graph.get_edges())
-    graph.make_matrix()
-    print(f"The matrix representation of the graph is:\n{graph.show_matrix()}")
+    g.add_edge((1, 4))
 
-    print(f'Path existence (1, 3): {graph.path_existence(1, 3)}')
+    print("Vertices: ", g.get_vertices())
+    print("Edges: ", g.get_edges())
+    g.make_matrix()
+    print(f"The matrix representation of the graph is:\n{g.show_matrix()}")
 
-    print(f'Path existence (2, 9): {graph.path_existence(2, 9)}')
+    print(f"Path existence (1, 3): {g.path_existence(1, 3)}")
 
-    print(f'Path existence (3, 5): {graph.path_existence(3, 5)}')
+    print(f"Path existence (2, 9): {g.path_existence(2, 9)}")
 
-    print(f'Path existence (1, 1): {graph.path_existence(1, 1)}')
+    print(f"Path existence (3, 5): {g.path_existence(3, 5)}")
 
-    print(f'Path existence (5, 5): {graph.path_existence(5, 5)}')
+    print(f"Path existence (1, 1): {g.path_existence(1, 1)}")
 
-    print(f'Path existence (10, 3): {graph.path_existence(10, 3)}')
+    print(f"Path existence (5, 5): {g.path_existence(5, 5)}")
 
-    print(f'Path existence (7, 9): {graph.path_existence(7, 9)}')
+    print(f"Path existence (10, 3): {g.path_existence(10, 3)}")
 
-    print(f'Path existence (4, 7): {graph.path_existence(4, 7)}')
+    print(f"Path existence (7, 9): {g.path_existence(7, 9)}")
 
-    print(f'Path existence ("2", "3"): {graph.path_existence("2", "3")}')
+    print(f"Path existence (4, 7): {g.path_existence(4, 7)}")
 
-    print(f'Path existence ("7", "9"): {graph.path_existence("7", "9")}')
+    print(f'Path existence ("2", "3"): {g.path_existence("2", "3")}')
+
+    print(f'Path existence ("7", "9"): {g.path_existence("7", "9")}')
+
+    print("For the Vertex(3)\n")
+    print(f"Total degrees: {g.degree(3)}")
+    print(f"In-degree: {g.idegree('3')}")
+    print(f'Out-degree: {g.odegree("3")}')
