5cdebc18dabb66a3578ad620a63c3807f52ea66c
MinhNQ <minhnguyenquang2004vn@gmail.com>
2025-01-06 01:42:49 +0700

    [test] some testing code

--- TP2.py
+++ TP2.py
@@ -27,6 +27,34 @@ if __name__ == "__main__":
 
     g.add_directed_edges(edge_names)
 
-    print(f"Directed matrix of g:\n{g.directed_matrix}")
+    print(f"Degree of Vertex(3) is: {g.degree(3)}\n")
+    print(f"Degree of Vertex(4) is: {g.degree('4')}\n")
+
+    print(f"Directed matrix of g:\n{g.show_directed_matrix()}")
     print(f"Number of directed edges: {len(g.edges)}")
-    print(f"Undirected matrix of g:\n{g.undirected_matrix}")
+    print(f"Undirected matrix of g:\n{g.show_undirected_matrix()}")
+
+    print(f"BFS of g from Vertex(1) is:\n{g.bfs_directed(1)}\n")
+    print(f"DFS of g from Vertex(1) is:\n{g.dfs_directed(1)}\n")
+
+    print(f"BFS result for the underlying undirected graph of g from Vertex(1) is:\n{g.bfs(1)}\n")
+    print(f"DFS result for the underlying undirected graph of g from Vertex(1) is:\n{g.dfs(1)}\n")
+
+    # find components within graph
+    g.find_components()
+    g.find_scc()
+    print(f"Weakly connected components in g:\n{g.components}")
+    for component in g.components:
+        print(f"---WCC vertices:{component.vertices}")
+        print(f"---WCC edges:{component.edges}")
+        print(f"---WCC's undirected matrix:\n{component.show_undirected_matrix()}\n")
+        print(f"---g's directed matrix:\n{g.show_directed_matrix()}\n")
+        print(f"---WCC's directed matrix:\n{component.show_directed_matrix()}\n")
+
+    print(f"Strongly connected components in g:\n{g.scc}")
+    for component in g.scc:
+        print('--------------------------------------------')
+        print(f"---SCC vertices:{component.vertices}")
+        print(f"---SCC edges:{component.edges}")
+        print(f"---SCC's undirected matrix:\n{component.show_undirected_matrix()}\n")
+        print(f"---SCC's directed matrix:\n{component.show_directed_matrix()}\n")
